31900b8d7ba6ebed4b93ead965621745635b7ed5
Zi <hungryzi@gmail.com>
2017-01-03 14:37:59 +0700

    Added links to search on book lending websites

--- js/content.js
+++ js/content.js
@@ -1,23 +1,56 @@
+function createLink(url, name) {
+  var anchor = document.createElement('a');
+  anchor.setAttribute('href', url);
+  anchor.setAttribute('class', 'a-size-mini')
+  anchor.setAttribute('target', '_blank')
+  var text = document.createTextNode('Search on ' + name);
+  anchor.appendChild(text);
+  return anchor;
+}
+
+function buildLendableContent(title) {
+  var encodedTitle = encodeURI('The Brothers Karamazov')
+
+  var booklendingUrl = 'http://www.booklending.com/borrow-book.htm?search=' + encodedTitle
+  var booklendingLink = createLink(booklendingUrl, 'BookLending.com')
+
+  var lendleUrl = 'http://lendle.me/books/available/?title=' + encodedTitle
+  var lendleLink = createLink(lendleUrl, 'Lendle.me')
+
+  var div = document.createElement('div')
+  div.setAttribute('style', 'padding: 0 11px;')
+
+  var title = document.createElement('h5')
+  title.setAttribute('style', 'color: #04b121;')
+  title.setAttribute('class', 'a-text-bold')
+  title.appendChild(document.createTextNode('Lendable book'))
+
+  div.appendChild(title)
+  div.appendChild(booklendingLink)
+  div.appendChild(document.createElement('br'))
+  div.appendChild(lendleLink)
+
+  return div
+}
+
 function modifyContent(text) {
-  // console.log('modifyContent', text)
-  var span = document.createElement('span');
-  var t = document.createTextNode(text);
-  btn.appendChild(t);
-  document.body.appendChild(btn);
+  var title = document.getElementById('ebooksProductTitle').innerText
+  var lendableContent = buildLendableContent(title)
+  var swatches = document.getElementById('tmmSwatches')
+  if (swatches && (divParent = swatches.getElementsByClassName('selected')[0])) {
+    divParent.appendChild(lendableContent)
+  }
 }
 
 if (detailsTable = document.getElementById('productDetailsTable')) {
-  // console.log('detailsTable', detailsTable)
   var items = detailsTable.getElementsByTagName('li');
-  // console.log('items', items)
-
   for (item of items) {
     if (item.innerText.match(/Lending/)) {
-      // console.log('Lending', item)
       var span = document.createElement('span');
       if (item.innerText.match(/Not Enabled/)) {
-        modifyContent('Not Enabled');
+        // POST as not lendable
       } else if (item.innerText.match(/Enabled/)) {
+        // POST as lendable
         modifyContent('Enabled');
       }
     }
